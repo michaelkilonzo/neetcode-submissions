64d42f7341973ae72c727fbea41046f06e31977d
michaelkilonzo <68613943+michaelkilonzo@users.noreply.github.com>
2026-04-30 14:16:59 -0400

    Add: singlyLinkedList - submission-3

---
+++ Data Structures & Algorithms/singlyLinkedList/submission-3.py
@@ -0,0 +1,63 @@
+class ListNode:
+    def __init__(self, val, nextNode=None):
+        self.val = val
+        self.next = nextNode
+
+class LinkedList:
+    
+    def __init__(self):
+        self.head = ListNode(float('-inf'))
+        self.tail = self.head
+
+    def get(self, index: int) -> int:
+        curr = self.head.next 
+        i = 0
+        while curr:
+            if i == index:
+                return curr.val 
+            i += 1
+            curr = curr.next
+        return -1
+
+    def insertHead(self, val: int) -> None:
+        newNode = ListNode(val)
+        newNode.next = self.head.next
+        self.head.next = newNode
+
+        if self.tail == self.head:
+            self.tail = newNode
+
+    def insertTail(self, val: int) -> None:
+        newNode = ListNode(val)
+
+        if self.tail == self.head:
+            self.tail.next = newNode
+            self.tail = newNode
+        else:
+            self.tail.next = newNode
+            self.tail = newNode
+
+    def remove(self, index: int) -> bool:
+        prev = self.head
+        i = 0
+
+        while i < index and prev.next:
+            i += 1
+            prev = prev.next
+        
+        if not prev.next:
+            return False
+
+        if prev.next == self.tail:
+            self.tail = prev
+
+        prev.next = prev.next.next
+        return True
+
+    def getValues(self) -> List[int]:
+        curr = self.head.next
+        res = []
+        while curr:
+            res.append(curr.val)
+            curr = curr.next
+        return res
